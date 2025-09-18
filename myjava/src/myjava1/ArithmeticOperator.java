da4c3b4b1cc9d94053d3dd10b804ad3fe7828a70
anirudh47 <22k91a6747@tkrcet.com>
2025-09-18 19:59:45 +0530

    the fourth commit

---
+++ myjava/src/myjava1/ArithmeticOperator.java
@@ -0,0 +1,17 @@
+package myjava1;
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		System.out.println("The addition of a+b="+ (a + b));
+		System.out.println("The substraction of a-b="+ (a - b));
+		System.out.println("The multiplication of a*b="+ (a * b));
+		System.out.println("The division of a/b="+ (a / b));
+		System.out.println("The modular of a%b="+ (a % b));
+		
+
+	}
+
+}
