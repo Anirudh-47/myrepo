a2e1cf34316a78e77f6188ec045ee132572eae76
anirudh47 <22k91a6747@tkrcet.com>
2025-09-18 20:47:11 +0530

    the sixth commit "PrimeNumber"

---
+++ myjava/src/myjava1/PrimeNumber.java
@@ -0,0 +1,25 @@
+package myjava1;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers from 1 to 100 are:");
+
+        for (int n = 2; n <= 100; n++) {
+            boolean isPrime = true;
+
+            // Check Divisibility from 2 to sort(n)
+            
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(n + " ");
+            }
+        }
+    }
+}
+
