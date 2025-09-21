e16a4b6e1bf24b975beadf9ebc75adf795f6dc5f
anirudh47 <22k91a6747@tkrcet.com>
2025-09-21 21:53:06 +0530

    commit "Hybrid Inheritance Example"

--- myjava/src/myjava1/Vehicle.java
+++ myjava/src/myjava1/Vehicle.java
@@ -1,5 +1,45 @@
 package myjava1;
 
-public class Vehicle {
+//Hybrid Inheritance Example 
 
+
+class Vehicle {
+ void fuel() {
+     System.out.println("Vehicles need fuel to run.");
+ }
 }
+
+
+class Car extends Vehicle {
+ void wheels() {
+     System.out.println("Car has 4 wheels.");
+ }
+}
+
+
+interface Electric {
+ void charge();
+}
+
+
+class ElectricCar extends Car implements Electric {
+ public void charge() {
+     System.out.println("ElectricCar charges using electricity.");
+ }
+}
+
+public class HybridExample {
+ public static void main(String[] args) {
+     ElectricCar tesla = new ElectricCar();
+
+   
+     tesla.fuel();
+
+    
+     tesla.wheels();
+
+    
+     tesla.charge();
+ }
+}
+
