cd0a3b807164e871a6ac9713ede103021f58a26c
anirudh47 <22k91a6747@tkrcet.com>
2025-09-21 21:48:00 +0530

    commit"Hierarchial Inheritance Example"

--- myjava/src/myjava1/Animal.java
+++ myjava/src/myjava1/Animal.java
@@ -1,5 +1,50 @@
 package myjava1;
 
-public class Animal {
+//Hierarchical Inheritance Example
 
+
+class Animal {
+ void eat() {
+     System.out.println("All animals eat food.");
+ }
+}
+
+
+class Dog extends Animal {
+ void bark() {
+     System.out.println("Dog barks.");
+ }
+}
+
+
+class Cat extends Animal {
+ void meow() {
+     System.out.println("Cat meows.");
+ }
+}
+
+
+class Cow extends Animal {
+ void moo() {
+     System.out.println("Cow moos.");
+ }
+}
+
+public class HierarchicalDemo {
+ public static void main(String[] args) {
+    
+     Dog d = new Dog();
+     d.eat();   
+     d.bark();
+
+
+     Cat c = new Cat();
+     c.eat(); 
+     c.meow();
+
+    
+     Cow cw = new Cow();
+     cw.eat();  
+     cw.moo();
+ }
 }
